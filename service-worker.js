dbbd6d733699cbf705ad82e56d35690105f5970f
abbamaster029-png <abbamaster029@gmail.com>
2026-08-14 16:11:36 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,61 @@
-const CACHE_NAME = "lvt-app-v1";
+const CACHE_NAME = "lvt-app-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./lvt-icon-192-2.png",
+  "./New Project 99 [0AAD052].png"
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(FILES_TO_CACHE))
   );
 
   self.skipWaiting();
+
 });
+
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+
+  event.waitUntil(
+
+    caches.keys().then(cacheNames => {
+
+      return Promise.all(
+
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+
+      );
+
+    }).then(() => {
+
+      return self.clients.claim();
+
+    })
+
+  );
+
 });
 
+
 self.addEventListener("fetch", event => {
+
   event.respondWith(
+
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
   );
+
 });
